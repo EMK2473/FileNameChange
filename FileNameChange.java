06d1e47410a5f97c2c7cbeb9d6939e69b3dda1ae
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-05 18:25:09 -0400

    init file commit

---
+++ FileNameChange.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FileNameChange {
+   public static void main(String[] args) throws IOException {
+      Scanner scnr = new Scanner(System.in);
+
+      
+   }
+}
